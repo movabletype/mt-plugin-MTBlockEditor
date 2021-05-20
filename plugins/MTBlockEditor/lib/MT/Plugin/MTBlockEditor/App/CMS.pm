2431f2efb6766af4d9c1106ed53488375c8f2d1b
Taku Amano <taku@taaas.jp>
2021-05-20 15:40:24 +0900

    Refactore MT::Plugin::MTBlockEditor::App::*

--- plugins/MTBlockEditor/lib/MT/Plugin/MTBlockEditor/App/CMS.pm
+++ plugins/MTBlockEditor/lib/MT/Plugin/MTBlockEditor/App/CMS.pm
@@ -1,4 +1,4 @@
-# Movable Type (r) (C) 2007-2019 Six Apart Ltd. All Rights Reserved.
+# Movable Type (r) (C) 2006-2020 Six Apart Ltd. All Rights Reserved.
 # This code cannot be redistributed without permission from www.sixapart.com.
 # For more information, consult your Movable Type license.
 
@@ -6,208 +6,242 @@ package MT::Plugin::MTBlockEditor::App::CMS;
 
 use strict;
 use warnings;
+use utf8;
 
-use MT::Plugin::MTBlockEditor qw(plugin blocks load_tmpl);
-use MT::Plugin::MTBlockEditor::App;
+use MT::Util qw(encode_html);
+use Class::Method::Modifiers qw(around);
+use MT::Plugin::MTBlockEditor qw(plugin blocks tmpl_param);
 
-sub edit_be_block {
-    my ( $app, $param ) = @_;
-
-    my $blog    = $app->blog;
-    my $blog_id = $blog ? $blog->id : 0;
-    return $app->permission_denied() if !$app->can_do('edit_be_blocks');
-
-    my $id = $app->param('id');
-    return $app->permission_denied() if $id && $id !~ m/\A[0-9]+\z/;
-
-    if ($id) {
-        my $obj
-            = MT->model('be_block')->load( { blog_id => $blog_id, id => $id } );
-        return $app->return_to_dashboard( redirect => 1 ) unless $obj;
-        while ( my ( $key, $val ) = each %{ $obj->column_values() } ) {
-            $param->{$key} ||= $val;
-        }
-        $param->{wrap_root_block} = 1 if $obj->root_block eq $obj->ROOT_BLOCK_DEFAULT;
-    }
-    else {
-        $param->{identifier} = '';
-    }
-
-    $param->{saved} = !!$app->param('saved');
-
-    $param->{shortcut_count_default} = MT::Plugin::MTBlockEditor->SHORTCUT_COUNT_DEFAULT;
-    my @block_types
-        = grep { $_->{identifier} ne $param->{identifier} } @{ blocks( { blog_id => $blog_id } ) };
-    $param->{block_types}      = \@block_types;
-    $param->{block_type_ids}   = [ map { $_->{type_id} } @block_types ];
-    $param->{max_icon_size}    = MT->model('be_block')->MAX_ICON_SIZE;
-    $param->{max_icon_size_hr} = ( MT->model('be_block')->MAX_ICON_SIZE / 1024 ) . 'KB';
-
-    $app->add_breadcrumb(
-        plugin()->translate("Custom Blocks"),
-        $app->uri(
-            mode => 'list',
-            args => {
-                _type   => 'be_block',
-                blog_id => $blog_id,
-            },
-        )
-    );
-
-    if ( $param->{id} ) {
-        $app->add_breadcrumb( $param->{label} );
-    }
-    else {
-        $app->add_breadcrumb( $app->translate('Create Custom Block') );
-    }
-
-    $app->setup_editor_param($param);
-    MT::Plugin::MTBlockEditor::App::load_extensions($param);
-    $app->build_page( load_tmpl('edit_block.tmpl'), $param );
-}
-
-sub cms_save_filter_be_block {
+sub init_app {
     my ( $cb, $app ) = @_;
 
-    $app->param(
-          root_block => $app->param('wrap_root_block')
-        ? MT->model('be_block')->ROOT_BLOCK_DEFAULT
-        : ''
-    );
-    $app->param( can_remove_block => $app->param('can_remove_block') ? 1 : 0 );
+    require MT::ContentFieldType::Common;
+    around 'MT::ContentFieldType::Common::html_text', sub {
+        my $orig = shift;
+        my ( $prop, $content_data, $app, $opts ) = @_;
 
-    1;
+        my $cb = $content_data->data->{ $prop->content_field_id . '_convert_breaks' };
+        return $orig->(@_) if $cb ne 'block_editor';
+
+        my $text = $content_data->data->{ $prop->content_field_id };
+        return '' unless defined $text;
+        $text = MT->apply_text_filters( $text, [$cb] );
+
+        if ( length $text > 40 ) {
+            return MT::Util::encode_html( substr( $text, 0, 40 ) ) . '...';
+        }
+        else {
+            return MT::Util::encode_html($text);
+        }
+    };
+
+    require MT::Asset::Image;
+    around 'MT::Asset::Image::as_html', sub {
+        my $orig   = shift;
+        my ($self) = @_;
+        my $html   = $orig->(@_);
+        my $app    = MT->instance;
+
+        if (   $app
+            && $app->can('param')
+            && ( $app->param('edit_field') || '' ) =~ m/^mt-block-editor-/ )
+        {
+            $html =~ s{img \K}{data-id="@{[$self->id]}" data-url="@{[$self->url]}" };
+        }
+
+        $html;
+    };
 }
 
-sub can_save_be_block {
-    my ( $eh, $app, $obj ) = @_;
-    my $author = $app->user;
-    return 1 if $author->is_superuser();
+sub insert_after {
+    my ( $tmpl, $id, $tokens ) = @_;
 
-    my $blog_id;
-    if ( defined $obj and ref $obj ) {
-        $blog_id = $obj->blog_id;
-    }
-    elsif ( defined $obj ) {
+    my $before = $id ? $tmpl->getElementById($id) : undef;
 
-        # we got the id of this block
-        my $loaded_obj = MT->model('be_block')->load($obj);
-        return 0 unless $loaded_obj;
-        $blog_id = $loaded_obj->blog_id;
-    }
-    elsif ( $app->blog ) {
-        $blog_id = $app->blog->id;
-    }
-    else {
-        $blog_id = 0;
+    if ( !ref $tokens ) {
+        $tokens = plugin()->load_tmpl($tokens)->tokens;
     }
 
-    return $author->permissions($blog_id)->can_do('edit_be_blocks');
+    foreach my $t (@$tokens) {
+        $tmpl->insertAfter( $t, $before );
+        $before = $t;
+    }
 }
 
-sub can_delete_be_block {
-    my ( $eh, $app, $obj ) = @_;
-    my $author = $app->user;
-    return 1 if $author->is_superuser();
+sub load_extensions {
+    my ($param) = @_;
 
-    $obj = MT->model('be_block')->load($obj) unless ref $obj;
-    my $blog_id = $obj ? $obj->blog_id : ( $app->blog ? $app->blog->id : 0 );
+    my $regs = MT::Component->registry( 'editors', 'block_editor' );
+    if ( $regs && ref $regs eq 'ARRAY' ) {
+        for my $reg (@$regs) {
+            my $plugin = $reg->{plugin};
+            my $tmpls  = $param->{block_editor_extensions} ||= {
+                templates  => [],
+                extensions => [],
+                config     => {},
+            };
 
-    return $author->permissions($blog_id)->can_do('edit_be_blocks');
+            foreach my $k ('extension') {
+                my $conf = $reg->{$k};
+                next unless defined $conf;
+                if ( !ref $conf ) {
+                    $conf = {
+                        template => $conf,
+                        order    => 5,
+                    };
+                }
+
+                if ( my $tmpl = $plugin->load_tmpl( $conf->{template} ) ) {
+                    push(
+                        @{ $tmpls->{ $k . 's' } },
+                        { %$conf, tmpl => $tmpl, v => $plugin->version }
+                    );
+                }
+            }
+
+            $tmpls->{config} = { %{ $tmpls->{config} }, %{ $reg->{'config'} } }
+                if $reg->{'config'};
+            delete $tmpls->{config}{plugin};
+        }
+    }
 }
 
-sub edit_be_config {
-    my ( $app, $param ) = @_;
+sub _template_param_edit_content {
+    my ( $id, $variant, $cb, $app, $param, $tmpl ) = @_;
 
     my $blog    = $app->blog;
     my $blog_id = $blog ? $blog->id : 0;
-    return $app->permission_denied() if !$app->can_do('edit_be_configs');
 
-    my $id = $app->param('id');
-    return $app->permission_denied() if $id && $id !~ m/\A[0-9]+\z/;
-
-    if ($id) {
-        my $obj
-            = MT->model('be_config')->load( { blog_id => $blog_id, id => $id } );
-        return $app->return_to_dashboard( redirect => 1 ) unless $obj;
-        while ( my ( $key, $val ) = each %{ $obj->column_values() } ) {
-            $param->{$key} ||= $val;
-        }
+    my $tmpl_param = tmpl_param();
+    while ( my ( $k, $v ) = each %$tmpl_param ) {
+        $param->{$k} = $v;
     }
 
-    $param->{saved}                  = !!$app->param('saved');
     $param->{shortcut_count_default} = MT::Plugin::MTBlockEditor->SHORTCUT_COUNT_DEFAULT;
-    $param->{block_types}            = blocks( { blog_id => $blog->id } );
+    my @block_types = grep { $_->{is_default_visible} } @{ blocks( { blog_id => $blog_id } ) };
+    $param->{block_types}    = \@block_types;
+    $param->{block_type_ids} = [ map { $_->{type_id} } @block_types ];
 
-    $app->add_breadcrumb(
-        plugin()->translate("Custom Block Presets"),
-        $app->uri(
-            mode => 'list',
-            args => {
-                _type   => 'be_config',
-                blog_id => $blog_id,
-            },
+    my %block_display_options_map;
+    for ( MT->model('be_config')->load( { blog_id => [ 0, $blog_id ] } ) ) {
+        $block_display_options_map{ $_->id } = $_->block_display_options;
+    }
+    $param->{block_display_options_map} = \%block_display_options_map;
+    $param->{loader_variant}            = $variant;
+
+    load_extensions($param);
+    insert_after( $tmpl, $id, 'loader.tmpl' );
+}
+
+sub template_param_edit_content_data {
+    my ( $cb, $app, $param, $tmpl ) = @_;
+
+    my %field_data = ();
+
+    my $content_type_id = $app->param('content_type_id')
+        or return;
+    my $content_type = MT::ContentType->load($content_type_id)
+        or return;
+    $field_data{content_type} = {
+        name      => $content_type->name,
+        unique_id => $content_type->unique_id,
+    };
+
+    # FIXME: The core of MT breaks the cache data
+    delete $content_type->{__cached_fields};
+    for my $field ( @{ $content_type->field_objs } ) {
+        $field_data{fields}{ $field->id } = {
+            unique_id => $field->unique_id,
+            name      => $field->name,
+        };
+    }
+
+    my $before = $tmpl->getElementById('entry-publishing-widget');
+    my $t
+        = $tmpl->createTextNode(
+        qq{<input type="hidden" id="be-field-data" value="@{[encode_html(MT::Util::to_json(\%field_data))]}" />}
+        );
+    $tmpl->insertAfter( $t, $before );
+
+    _template_param_edit_content( 'content_data', '_content_data', @_ );
+}
+
+sub template_param_edit_entry {
+    my ( $cb, $app, $param, $tmpl ) = @_;
+
+    my $blog      = $app->blog;
+    my $config_id = int(
+        (     $param->{object_type} eq 'entry'
+            ? $blog->be_entry_config_id
+            : $blog->be_page_config_id
         )
+            || 0
     );
-    if ( $param->{id} ) {
-        $app->add_breadcrumb( $param->{label} );
-    }
-    else {
-        $app->add_breadcrumb( $app->translate('Create Custom Block Preset') );
-    }
 
-    $app->build_page( load_tmpl('edit_config.tmpl'), $param );
+    my $before = $tmpl->getElementById('text');
+    my $t
+        = $tmpl->createTextNode(qq{<input type="hidden" id="text-be_config" value="$config_id" />});
+    $tmpl->insertAfter( $t, $before );
+
+    _template_param_edit_content( 'text', '_entry', @_ );
 }
 
-sub can_save_be_config {
-    my ( $eh, $app, $obj ) = @_;
-    my $author = $app->user;
-    return 1 if $author->is_superuser();
+sub template_param_cfg_entry {
+    my ( $cb, $app, $param, $tmpl ) = @_;
 
-    my $blog_id;
-    if ( defined $obj and ref $obj ) {
-        $blog_id = $obj->blog_id;
+    my $blog = $app->blog;
+    for my $k (qw(be_entry_config_id be_page_config_id)) {
+        $param->{$k} = $blog->$k;
     }
-    elsif ( defined $obj ) {
+    $param->{be_configs} = [ MT->model('be_config')->load( { blog_id => [ 0, $blog->id ] } ) ];
 
-        # we got the id of this block
-        my $loaded_obj = MT->model('be_config')->load($obj);
-        return 0 unless $loaded_obj;
-        $blog_id = $loaded_obj->blog_id;
-    }
-    elsif ( $app->blog ) {
-        $blog_id = $app->blog->id;
-    }
-    else {
-        $blog_id = 0;
-    }
-
-    return $author->permissions($blog_id)->can_do('edit_be_configs');
+    insert_after( $tmpl, 'wysiwyg-editor-setting', 'cfg_entry.tmpl' );
 }
 
-sub can_delete_be_config {
-    my ( $eh, $app, $obj ) = @_;
-    my $author = $app->user;
-    return 1 if $author->is_superuser();
+sub template_source_multi_line_text {
+    my ( $cb, $app, $tmpl ) = @_;
 
-    $obj = MT->model('be_config')->load($obj) unless ref $obj;
-    my $blog_id = $obj ? $obj->blog_id : ( $app->blog ? $app->blog->id : 0 );
+    my $blog    = $app->blog;
+    my $blog_id = $blog ? $blog->id : 0;
 
-    return $author->permissions($blog_id)->can_do('edit_be_configs');
+    my $configs = [ MT->model('be_config')->load( { blog_id => [ 0, $blog_id ] } ) ];
+
+    return unless @$configs;
+
+    $$tmpl =~ s{(</mtapp:ContentFieldOptionScript>)}{
+this.options.be_configs = {
+@{[ map { my $id = $_->id; qq{$id: '',} } @$configs ]}
+};
+if ( this.options.be_config ) {
+  this.options.be_configs[this.options.be_config] = "selected"
 }
 
-sub cms_pre_save_blog {
-    my ( $eh, $app, $obj ) = @_;
+$1
+}i;
 
-    if ( $app->can_do('save_blog_config') ) {
-        for my $k (qw(be_entry_config_id be_page_config_id)) {
-            if ( defined( my $id = $app->param($k) ) ) {
-                $obj->$k($id);
-            }
-        }
-    }
+    $$tmpl =~ s{(</mt:app:ContentFieldOptionGroup>)}{
+<__trans_section component="@{[plugin()->id]}">
+<mtapp:ContentFieldOption
+   id="multi_line_text-be_config"
+   label="<__trans phrase="Preset For Movable Type Block Editor">">
+  <select ref="be_config" name="be_config" id="multi_line_text-be_config" class="custom-select form-control">
+    @{[ map { my ($id, $label) = ($_->id, encode_html($_->label)); qq{
+      <option value="$id" selected={ options.be_configs[$id] }>$label</option>
+    } } @$configs ]}
+  </select>
+</mtapp:ContentFieldOption>
+</__trans_section>
 
-    1;
+$1
+}i;
+}
+
+sub template_source_field_html_multi_line_text {
+    my ( $cb, $app, $tmpl ) = @_;
+
+    $$tmpl .= <<MTML;
+<input type="hidden" id="content-field-<mt:var name="content_field_id">-be_config" value="<mt:var name="options{be_config}" escape="html">" />
+MTML
 }
 
 1;
---
+++ plugins/MTBlockEditor/lib/MT/Plugin/MTBlockEditor/App/Config.pm
@@ -0,0 +1,107 @@
+# Movable Type (r) (C) 2007-2019 Six Apart Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+
+package MT::Plugin::MTBlockEditor::App::Config;
+
+use strict;
+use warnings;
+use utf8;
+
+use MT::Plugin::MTBlockEditor qw(plugin blocks load_tmpl);
+
+sub edit_be_config {
+    my ( $app, $param ) = @_;
+
+    my $blog    = $app->blog;
+    my $blog_id = $blog ? $blog->id : 0;
+    return $app->permission_denied() if !$app->can_do('edit_be_configs');
+
+    my $id = $app->param('id');
+    return $app->permission_denied() if $id && $id !~ m/\A[0-9]+\z/;
+
+    if ($id) {
+        my $obj
+            = MT->model('be_config')->load( { blog_id => $blog_id, id => $id } );
+        return $app->return_to_dashboard( redirect => 1 ) unless $obj;
+        while ( my ( $key, $val ) = each %{ $obj->column_values() } ) {
+            $param->{$key} ||= $val;
+        }
+    }
+
+    $param->{saved}                  = !!$app->param('saved');
+    $param->{shortcut_count_default} = MT::Plugin::MTBlockEditor->SHORTCUT_COUNT_DEFAULT;
+    $param->{block_types}            = blocks( { blog_id => $blog->id } );
+
+    $app->add_breadcrumb(
+        plugin()->translate("Custom Block Presets"),
+        $app->uri(
+            mode => 'list',
+            args => {
+                _type   => 'be_config',
+                blog_id => $blog_id,
+            },
+        )
+    );
+    if ( $param->{id} ) {
+        $app->add_breadcrumb( $param->{label} );
+    }
+    else {
+        $app->add_breadcrumb( $app->translate('Create Custom Block Preset') );
+    }
+
+    $app->build_page( load_tmpl('edit_config.tmpl'), $param );
+}
+
+sub can_save_be_config {
+    my ( $eh, $app, $obj ) = @_;
+    my $author = $app->user;
+    return 1 if $author->is_superuser();
+
+    my $blog_id;
+    if ( defined $obj and ref $obj ) {
+        $blog_id = $obj->blog_id;
+    }
+    elsif ( defined $obj ) {
+
+        # we got the id of this block
+        my $loaded_obj = MT->model('be_config')->load($obj);
+        return 0 unless $loaded_obj;
+        $blog_id = $loaded_obj->blog_id;
+    }
+    elsif ( $app->blog ) {
+        $blog_id = $app->blog->id;
+    }
+    else {
+        $blog_id = 0;
+    }
+
+    return $author->permissions($blog_id)->can_do('edit_be_configs');
+}
+
+sub can_delete_be_config {
+    my ( $eh, $app, $obj ) = @_;
+    my $author = $app->user;
+    return 1 if $author->is_superuser();
+
+    $obj = MT->model('be_config')->load($obj) unless ref $obj;
+    my $blog_id = $obj ? $obj->blog_id : ( $app->blog ? $app->blog->id : 0 );
+
+    return $author->permissions($blog_id)->can_do('edit_be_configs');
+}
+
+sub cms_pre_save_blog {
+    my ( $eh, $app, $obj ) = @_;
+
+    if ( $app->can_do('save_blog_config') ) {
+        for my $k (qw(be_entry_config_id be_page_config_id)) {
+            if ( defined( my $id = $app->param($k) ) ) {
+                $obj->$k($id);
+            }
+        }
+    }
+
+    1;
+}
+
+1;
